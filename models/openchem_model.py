5e4c2bbeef9e09de764fafc5a73ef095f0333ab9
mariewelt <mariewelt@gmail.com>
2018-07-11 11:25:04 -0700

    multiprocessing support

--- models/openchem_model.py
+++ models/openchem_model.py
@@ -1,8 +1,7 @@
 import torch
 from torch import nn
-from torch.nn.utils import clip_grad_norm
+from torch.nn.utils import clip_grad_norm, clip_grad_norm_
 
-from data.utils import create_loader
 from utils.utils import check_params
 
 import os
@@ -33,22 +32,24 @@ class OpenChemModel(nn.Module):
         self.optimizer = None
         self.lr_scheduler = self.params['lr_scheduler']
         self.logdir = self.params['logdir']
+        self._flat_grads = None
+        self.distributed_world_size = self.params['world_size']
 
         # check if log directory exists, create if it doesn't
-        try:
-            os.stat(self.logdir)
-        except UserWarning(self.logdir + ' directory not found. '
-                                         'Creating...'):
-            os.mkdir(self.logdir)
-            print('Directory created')
+        # try:
+        #    os.stat(self.logdir)
+        # except UserWarning(self.logdir + ' directory not found. '
+        #                                  'Creating...'):
+        #     os.mkdir(self.logdir)
+        #     print('Directory created')
         # check if folder checkpoint within log directory exists,
         # create if it doesn't
-        try:
-            os.stat(self.logdir + '/checkpoint')
-        except UserWarning(self.logdir + '/checkpoint directory not found. '
-                                         'Creating...'):
-            os.mkdir(self.logdir + '/checkpoint')
-            print('Directory created')
+        # try:
+        #    os.stat(self.logdir + '/checkpoint')
+        # except UserWarning(self.logdir + '/checkpoint directory not found. '
+        #                                  'Creating...'):
+        #     os.mkdir(self.logdir + '/checkpoint')
+        #     print('Directory created')
 
         self.num_epochs = self.params['num_epochs']
         self.use_clip_grad = self.params['use_clip_grad']
@@ -117,18 +118,25 @@ class OpenChemModel(nn.Module):
     def cast_inputs(self, sample):
         raise NotImplementedError
 
-    def train_step(self, inp, target):
+    def train_step(self, inp, target, multiprocess=False):
         self.optimizer.zero_grad()
         output = self.forward(inp, eval=False)
         loss = self.criterion(output, target)
         loss.backward()
-        self.optimizer.step()
-        if self.use_clip_grad:
-            clip_grad_norm(self.parameters(), self.max_grad_norm)
+        if multiprocess:
+            try:
+                self._all_reduce_and_rescale()
+            except OverflowError as e:
+                self.zero_grad()
+                print('| WARNING: overflow detected, ' + str(e))
+        else:
+            self.optimizer.step()
+            if self.use_clip_grad:
+                clip_grad_norm(self.parameters(), self.max_grad_norm)
 
         return loss.item()
 
-    def fit(self, eval=True):
+    def fit(self, eval=True, multiprocess=False):
         cur_epoch = 0
         logdir = self.logdir
         print_every = self.print_every
@@ -145,7 +153,7 @@ class OpenChemModel(nn.Module):
         for epoch in range(cur_epoch, n_epochs + cur_epoch):
             for i_batch, sample_batched in enumerate(self.train_loader):
                 batch_input, batch_target = self.cast_inputs(sample_batched)
-                loss = self.train_step(batch_input, batch_target)
+                loss = self.train_step(batch_input, batch_target, multiprocess)
                 loss_total += loss
                 n_batches += 1
             cur_loss = loss_total / n_batches
@@ -214,3 +222,52 @@ class OpenChemModel(nn.Module):
 
     def save_model(self, path):
         torch.save(self.state_dict(), path)
+
+    def _all_reduce_and_rescale(self):
+        # modified from fairseq
+        # flatten grads into a single buffer and all-reduce
+        flat_grads = self._flat_grads = self._get_flat_grads(
+            self._flat_grads)
+        if self.distributed_world_size > 1:
+            torch.distributed.all_reduce(flat_grads)
+
+        # rescale and clip gradients
+        if self.use_clip_grad:
+            clip_grad_norm_(flat_grads, self.max_grad_norm)
+
+        # copy grads back into model parameters
+        self._set_flat_grads(flat_grads)
+
+    def _get_grads(self):
+        grads = []
+        for name, p in self.named_parameters():
+            if not p.requires_grad:
+                continue
+            if p.grad is None:
+                raise RuntimeError(
+                    'Model parameter did not receive gradient: ' + name + '. '
+                    'Use the param in the forward pass or set '
+                    'requires_grad=False')
+            grads.append(p.grad.data)
+        return grads
+
+    def _get_flat_grads(self, out=None):
+        grads = self._get_grads()
+        if out is None:
+            grads_size = sum(g.numel() for g in grads)
+            out = grads[0].new(grads_size).zero_()
+        offset = 0
+        for g in grads:
+            numel = g.numel()
+            out[offset:offset + numel].copy_(g.view(-1))
+            offset += numel
+        return out[:offset]
+
+    def _set_flat_grads(self, new_grads):
+        grads = self._get_grads()
+        offset = 0
+        for g in grads:
+            numel = g.numel()
+            g.copy_(new_grads[offset:offset + numel].view_as(g))
+
+        offset += numel
