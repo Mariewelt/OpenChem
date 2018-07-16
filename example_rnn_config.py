0eb1f0a6e6c3dcab8805c2213386cc27f4646eb4
Mariya Popova <mariewelt@gmail.com>
2018-07-16 16:00:16 -0700

    added small changes

--- example_rnn_config.py
+++ example_rnn_config.py
@@ -14,7 +14,8 @@ from sklearn.metrics import roc_auc_score
 train_dataset = SmilesDataset('./benchmark_datasets/HIV_dataset/HIV_train.csv',
                               cols_to_read=[0, 1])
 val_dataset = SmilesDataset('./benchmark_datasets/HIV_dataset/HIV_test.csv',
-                            cols_to_read=[0, 1])
+                            cols_to_read=[0, 1], tokens=train_dataset.tokens)
+assert train_dataset.tokens == val_dataset.tokens
 
 use_cuda = True
 
@@ -25,10 +26,10 @@ model_params = {
     'task': 'classification',
     'random_seed': 5,
     'use_clip_grad': True,
-    'max_grad_norm': 10.0,
+    'max_grad_norm': 1.0,
     'batch_size': 128,
-    'num_epochs': 100,
-    'logdir': '/home/mpopova/Work/OpenChem/logs',
+    'num_epochs': 200,
+    'logdir': '/home/mpopova/Work/OpenChem/logs/rnn_log',
     'print_every': 1,
     'save_every': 5,
     'train_data_layer': train_dataset,
@@ -37,7 +38,8 @@ model_params = {
     'criterion': nn.CrossEntropyLoss(),
     'optimizer': RMSprop,
     'optimizer_params': {
-        'lr': 0.001
+        'lr': 0.0001,
+	'weight_decay': 1e-5
     },
     'lr_scheduler': ExponentialLR,
     'lr_scheduler_params': {
@@ -46,24 +48,24 @@ model_params = {
     'embedding': Embedding,
     'embedding_params': {
         'num_embeddings': max(train_dataset.num_tokens, val_dataset.num_tokens),
-        'embedding_dim': 256,
+        'embedding_dim': 128,
         'padding_idx': 0
     },
     'encoder': RNNEncoder,
     'encoder_params': {
-        'input_size': 256,
+        'input_size': 128,
         'layer': "LSTM",
-        'encoder_dim': 512,
+        'encoder_dim': 64,
         'n_layers': 2,
-        'dropout': 0.3,
+        'dropout': 0.8,
         'is_bidirectional': False
     },
     'mlp': OpenChemMLP,
     'mlp_params': {
-        'input_size': 512,
+        'input_size': 64,
         'n_layers': 2,
-        'hidden_size': [256, 2],
+        'hidden_size': [64, 2],
         'activations': [F.relu, F.relu],
-        'dropouts': [0.3, 0.3]
+        'dropout': 0.8
     }
 }
