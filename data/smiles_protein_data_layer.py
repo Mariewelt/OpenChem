c0abe55fc7bdf468415c0dcb4f637fcfea71fc3d
mariewelt <mariewelt@gmail.com>
2018-07-16 15:57:18 -0700

    data layer for molecules-protein model

--- data/smiles_protein_data_layer.py
+++ data/smiles_protein_data_layer.py
@@ -1,6 +1,7 @@
 # TODO: packed variable length sequence
 
 import numpy as np
+import pickle
 
 from torch.utils.data import Dataset
 
@@ -10,27 +11,34 @@ from data.utils import tokenize
 
 
 class SmilesProteinDataset(Dataset):
-    def __init__(self, filename, cols_to_read, delimiter=',', mol_tokens=None,
-                 prot_tokens=None, pad=True):
+    def __init__(self, filename, tokenized=False, cols_to_read=None,
+                 delimiter=',', mol_tokens=None, prot_tokens=None, pad=True):
         super(SmilesProteinDataset, self).__init__()
-        data = read_smiles_property_file(filename, cols_to_read, delimiter)
-        smiles = data[0]
-        proteins = np.array(data[1])
-        target = np.array(data[2], dtype='float')
-        clean_smiles, clean_idx = sanitize_smiles(smiles)
-        self.target = target[clean_idx]
-        proteins = list(proteins[clean_idx])
-        if pad:
-            clean_smiles = pad_sequences(clean_smiles)
-            proteins = pad_sequences(proteins)
-        self.mol_tokens, self.mol_token2idx, self.mol_num_tokens = \
-            tokenize(clean_smiles, mol_tokens)
-        self.prot_tokens, self.prot_token2idx, self.prot_num_tokens = \
-            tokenize(proteins, prot_tokens)
-        clean_smiles = seq2tensor(clean_smiles, self.mol_tokens)
-        proteins = seq2tensor(proteins, self.prot_tokens)
-        self.molecules = clean_smiles
-        self.proteins = proteins
+        if not tokenized:
+            data = read_smiles_property_file(filename, cols_to_read, delimiter)
+            smiles = data[0]
+            proteins = np.array(data[1])
+            target = np.array(data[2], dtype='float')
+            clean_smiles, clean_idx = sanitize_smiles(smiles)
+            self.target = target[clean_idx]
+            proteins = list(proteins[clean_idx])
+            if pad:
+                clean_smiles = pad_sequences(clean_smiles)
+                proteins = pad_sequences(proteins)
+            self.mol_tokens, self.mol_token2idx, self.mol_num_tokens = \
+                tokenize(clean_smiles, mol_tokens)
+            self.prot_tokens, self.prot_token2idx, self.prot_num_tokens = \
+                tokenize(proteins, prot_tokens)
+            clean_smiles = seq2tensor(clean_smiles, self.mol_tokens)
+            proteins = seq2tensor(proteins, self.prot_tokens)
+            self.molecules = clean_smiles
+            self.proteins = proteins
+        else:
+            f = open(filename, 'rb')
+            data = pickle.load(f)
+            self.molecules = data['smiles_tensor']
+            self.proteins = data['protein_tensor']
+            self.target = data['labels']
         assert len(self.molecules) == len(self.proteins)
         assert len(self.molecules) == len(self.target)
 
