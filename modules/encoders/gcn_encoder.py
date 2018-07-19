ac9ad35dd3dcc619e5c9210c96756f409dc4cde6
Mariya Popova <mariewelt@gmail.com>
2018-07-19 11:20:50 -0700

    added graph pooling

--- modules/encoders/gcn_encoder.py
+++ modules/encoders/gcn_encoder.py
@@ -17,9 +17,11 @@ class GraphCNNEncoder(OpenChemEncoder):
         self.hidden_size = params['hidden_size']
         self.dropout = params['dropout']
         assert len(self.hidden_size) == self.n_layers
-        self.hidden_size += [self.encoder_dim]
+        #self.hidden_size += [self.encoder_dim]
         self.hidden_size = [self.input_size] + self.hidden_size
         self.graph_convolutions = nn.ModuleList()
+        self.dense = nn.Linear(in_features=self.hidden_size[-1],
+                                      out_features=self.encoder_dim)
         for i in range(1, self.n_layers+1):
             self.graph_convolutions.append(GraphConvolution(self.
                                                             hidden_size[i-1],
@@ -44,8 +46,10 @@ class GraphCNNEncoder(OpenChemEncoder):
         adj = inp[1]
         for i in range(self.n_layers):
             x = self.graph_convolutions[i](x, adj)
-            if i < self.n_layers - 1:
-                x = F.dropout(x, self.dropout, training=self.training)
+            #if i < self.n_layers - 1:
+            #    x = F.dropout(x, self.dropout)
             x = F.relu(x)
-        x = x.mean(dim=1)
+            x = torch.bmm(adj, x)
+        x = F.tanh(self.dense(x))
+        x = F.tanh(x.sum(dim=1))
         return x
