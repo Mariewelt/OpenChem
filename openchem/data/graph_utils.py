2933754902b9dceb4da5f63c42775db9a909c0f9
marieweltm <mariewelt@gmail.com>
2019-04-17 21:39:53 +0000

    Added function for converting graphs into SMILES to graph utils

--- openchem/data/graph_utils.py
+++ openchem/data/graph_utils.py
@@ -4,6 +4,8 @@
 import networkx as nx
 import numpy as np
 
+from rdkit import Chem
+
 
 def bfs_seq(G, start_id):
     '''
@@ -56,3 +58,53 @@ def encode_adj(adj, max_prev_node=10, is_full = False):
         adj_output[i,:] = adj_output[i,:][::-1] # reverse order
 
     return adj_output
+
+
+def SmilesFromGraphs(node_list, adjacency_matrix):
+    """
+    Converts molecular graph to SMILES string
+    :param node_list:
+    :param adjacency_matrix:
+    :return:
+    """
+    # create empty editable mol object
+    mol = Chem.RWMol()
+
+    # add atoms to mol and keep track of index
+    node_to_idx = {}
+    for i in range(len(node_list)):
+        a = Chem.Atom(node_list[i])
+        molIdx = mol.AddAtom(a)
+        node_to_idx[i] = molIdx
+
+    # add bonds between adjacent atoms
+    for ix, row in enumerate(adjacency_matrix):
+        for iy, bond in enumerate(row):
+
+            # only traverse half the matrix
+            if iy <= ix:
+                continue
+
+            # add relevant bond type (there are many more of these)
+            if bond == 0.:
+                continue
+            elif bond == 1.:
+                bond_type = Chem.rdchem.BondType.SINGLE
+                mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+            elif bond == 1.5:
+                bond_type = Chem.rdchem.BondType.AROMATIC
+                mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+            elif bond == 2.:
+                bond_type = Chem.rdchem.BondType.DOUBLE
+                mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+            elif bond == 3.:
+                bond_type = Chem.rdchem.BondType.TRIPLE
+                mol.AddBond(node_to_idx[ix], node_to_idx[iy], bond_type)
+
+    # Convert RWMol to Mol object
+    mol = mol.GetMol()
+
+    # Convert RWMol to SMILES
+    smiles = Chem.MolToSmiles(mol)
+
+    return smiles
