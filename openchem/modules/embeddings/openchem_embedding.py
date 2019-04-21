e71a6329f8ed82ba539a1687444c6e0d536318b9
marieweltm <mariewelt@gmail.com>
2019-04-21 19:10:22 +0000

    Added onehot embedding

--- openchem/modules/embeddings/openchem_embedding.py
+++ openchem/modules/embeddings/openchem_embedding.py
@@ -10,28 +10,24 @@ class OpenChemEmbedding(nn.Module):
                      self.get_optional_params())
         self.params = params
         self.num_embeddings = self.params['num_embeddings']
-        self.embedding_dim = self.params['embedding_dim']
         if 'padding_idx' in params.keys():
             self.padding_idx = self.params['padding_idx']
         else:
             self.padding_idx = None
-        self.embedding = nn.Embedding(num_embeddings=self.num_embeddings,
-                                      embedding_dim=self.embedding_dim,
-                                      padding_idx=self.padding_idx)
+
 
     def forward(self, inp):
         raise NotImplementedError
 
     @staticmethod
-    def get_optional_params():
-        return {
-            'padding_idx': int
-        }
-
-    @staticmethod
     def get_required_params():
         return {
             'num_embeddings': int,
-            'embedding_dim': int
         }
 
+    @staticmethod
+    def get_optional_params():
+        return {
+            'padding_idx': int
+
+        }
