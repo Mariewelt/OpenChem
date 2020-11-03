c290c9d66107261f8e256eb0b8aa6a594004f34c
Mariewelt <mariewelt@gmail.com>
2020-11-03 13:43:03 -0800

    updated setup.py file

--- setup.py
+++ setup.py
@@ -1,50 +1,33 @@
 """
-OpenChem -- Deep Learning toolkit for Cheminformatics
+OpenChem -- Deep Learning toolkit for Computational Chemistry and Drug Design
 """
-from setuptools import setup
-import versioneer
 
-short_description = __doc__.split("\n")
+from setuptools import setup, find_packages
 
-try:
-    with open("README.md", "r") as handle:
-        long_description = handle.read()
-except:
-    long_description = "\n".join(short_description[2:]),
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
+setup_attrs = {
+    'name': 'OpenChem',
+    'description': 'Deep Learning toolkit for Computational Chemistry and Drug Design with PyTorch backend',
+    'long_description': long_description,
+    'long_description_content_type': "text/markdown",
+    'url': 'https://github.com/Mariewelt/OpenChem',
+    'author': 'Mariewelt',
+    'author_email': 'mariewelt@gmail.com',
+    'license': 'MIT',
+    'packages': find_packages(),
+    'include_package_data': True,
+    'use_scm_version': True,
+    'setup_requires': ['setuptools_scm'],
+    'install_requires': [
+        'tensorboardX',
+        'networkx',
+        'tqdm',
+        'torchani'
+    ],
+    'python_requires': ">=3.5",
+    'zip_safe': False
+}
 
-setup(
-    # Self-descriptive entries which should always be present
-    name='openchem',
-    author='mariewelt',
-    author_email='mariewelt@gmail.com',
-    description=short_description[0],
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    license='BSD-3-Clause',
-
-    # Which Python importable modules should be included when your package is installed
-    packages=['openchem', "openchem.tests"],
-
-    # Optional include package data to ship with your package
-    # Comment out this line to prevent the files from being packaged with your software
-    # Extend/modify the list to include/exclude other items as need be
-    package_data={'openchem': ["data/*.dat"]
-                  },
-
-    # Additional entries you may want simply uncomment the lines you want and fill in the data
-    # author_email='me@place.org',      # Author email
-    # url='http://www.my_package.com',  # Website
-    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
-    # platforms=['Linux',
-    #            'Mac OS-X',
-    #            'Unix',
-    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
-    # python_requires=">=3.5",          # Python version restrictions
-
-    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
-    # zip_safe=False,
-
-)
+setup(**setup_attrs)
