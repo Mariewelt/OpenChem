afd5713cf751b53a4762d9e5bda22f86a0b03041
Mariewelt <mariewelt@gmail.com>
2020-11-03 14:50:37 -0800

    added by iteration propoerty

--- openchem/optimizer/openchem_lr_scheduler.py
+++ openchem/optimizer/openchem_lr_scheduler.py
@@ -1,12 +1,19 @@
 class OpenChemLRScheduler(object):
-
     def __init__(self, params, optimizer):
         self.params = params[1]
+        self._by_iter = self.params.pop("by_iter", False)
         self._scheduler = params[0](optimizer, **self.params)
 
     @property
     def scheduler(self):
         return self._scheduler
+
+    @property
+    def by_iteration(self):
+        """
+        If true, make step every iteration, else every epoch (default)
+        """
+        return self._by_iter
 
     def step(self):
         """Performs a single scheduler step."""
